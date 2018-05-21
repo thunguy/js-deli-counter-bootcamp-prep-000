69418776b9ac95e11678ff91a1665d5c6ed0221b
thunguy@me.com <thunguy@me.com>
2018-05-21 00:51:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(katzDeliLine, nextName) {
+  katzDeliLine.push(nextName);
+  
+  return (`Welcome, ${nextName}. You are number ${katzDeliLine.length} in line.`)
+}
+
+function nowServing(name) {
+  if (name.length === 0) {
+    return (`There is nobody waiting to be served!`)
+  }
+  
+  return (`Currently serving ${name.shift()}.`)
+}
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return (`The line is currently empty.`)
+  }
+  
+  var numAndName = `The line is currently: `
+  
+  for (let i = 0; i < line.length; i++) { 
+    numAndName = numAndName + i + `. ${line[i]}`
+    
+    return numAndName
+  } 
+}  
